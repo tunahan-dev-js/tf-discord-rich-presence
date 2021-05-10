6e13a60b472b248747005e22004273674c742723
Stronger Developer <83252318+StrongerDeveloper@users.noreply.github.com>
2021-05-10 18:46:42 +0300

    Update index.js
    Sistem Güncellendi.

--- index.js
+++ index.js
@@ -1,33 +1,50 @@
-const rpc = require("discord-rpc");
-const client = new rpc.Client({ transport: "ipc" });
-const chalk = require("chalk");
+const rpc = require("discord-rpc"),
+client = new rpc.Client({ transport: "ipc" }),
+chalk = require("chalk"),
+ClientID = "";
 
 client.on("ready", async() => {
-    console.log(chalk.bold.green("[TF] | Hesabınıza Bağlanıldı."));
+//////////////////////////////////////////////////////////////////////////////////////////////////
+    console.log(chalk.bold.green(`[TF] | ${client.user.username} Adlı Hesaba Bağlanıldı.`));
+	const BüyükResim = "tf_logo",
+	BüyükResimAçıklaması = "Türk Federasyonu [TF]", 
+	KüçükResim = "tf_simgesellogo_beyaz", 
+	KüçükResimAçıklaması= "Biz, Her Yerdeyiz!",
+    Açıklama1 = "Karanlığa Çak Bir Kibrit!",
+    Açıklama2 = "Ne Mutlu Türk'üm Diyene!";
+//////////////////////////////////////////////////////////////////////////////////////////////////
     try {
         await client.request("SET_ACTIVITY", {
             pid: process.pid,
             activity: {
-                details: "D E V R İ M | Karanlığa Çak Bir Kibrit!", // Durum Açıklaması
+                details: Açıklama1,
+                state: Açıklama2,
                 assets: {
-                    large_image: "tf_logo", // Büyük Resim İsmi
-                    large_text: "Türk Federasyonu [TF]", // Büyük Resim Açıklaması
-                    small_image: "tf_simgesellogo_beyaz", // Küçük Resim İsmi
-                    small_text: "Biz, Her Yerdeyiz!" // Küçük Resim İsmi
+                    large_image: BüyükResim,
+                    large_text: BüyükResimAçıklaması,
+                    small_image: KüçükResim,
+                    small_text: KüçükResimAçıklaması
                 },
-                buttons: [{ label: "[TF] | Ana Merkezi", url: "https://turkfederasyonu.org/tffb/" }, { label: "[TF] | Discord Merkezi", url: "https://discord.gg/W3TYTY5" }] // Bağlantılar (Label: Bağlantı Adı | URL: Bağlantı URL'si)
+                timestamps: {
+                    start: new Date().getTime()
+                },
+                buttons: 
+                [
+                    { label: "[TF] | Ana Merkezi", url: "https://turkfederasyonu.org/tffb/" }, 
+                    { label: "[TF] | Discord Merkezi", url: "https://discord.gg/W3TYTY5" }
+                ] // Butonlar (Label: Buton Adı | URL: Buton Bağlantısı)
             }
         })
         .then(() => {
             console.log(chalk.bold.green("[TF] | Durumunuz Başarıyla Ayarlandı."));
         })
     } catch(error) {
-        console.log(chalk.bold.red("[TF] | Durumunuz Ayarlanırken Bir Sorun ile Karşılaşıldı."));
+        console.log(chalk.bold.red("[TF] | Durumunuz Ayarlanırken Bir Sorun ile Karşılaşıldı.", error));
     }
+//////////////////////////////////////////////////////////////////////////////////////////////////
 })
 
-client.login({ clientId: "ClientID" }).catch((error) => {
-    if(error) {
-        console.log(chalk.bold.red("[TF] | Hesabınıza Bağlanılamadı."));
-    }
+client.login({ clientId: ClientID })
+.catch((error) => {
+    console.log(chalk.bold.red(`[TF] | ${client.user.username} Adlı Hesaba Bağlanılamadı.`, error));
 })
